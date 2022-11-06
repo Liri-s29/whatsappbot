c05847a93ab0630834cb4955e759c9237e797f56
Srihari S <srihari.sundaramurugan@gmail.com>
2022-11-06 16:12:39 +0530

    cc

--- modules/chat/service/get-all-messages.js
+++ modules/chat/service/get-all-messages.js
@@ -8,7 +8,7 @@ module.exports = async function getAllMessages() {
 		chats.forEach((chat) => {
 			chat.messages.forEach((message) => {
 				messages.push({
-					text: message.text,
+					text: message.message,
 				});
 			});
 		});
